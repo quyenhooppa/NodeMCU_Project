b232aeeee8237ede6ec1db66daee39b09142a3b1
trkhoa1799 <truongkhoa.1799@gmail.com>
2019-06-03 20:57:39 +0700

    6-3-19

--- PCMT.ino
+++ PCMT.ino
@@ -4,15 +4,14 @@
 #include <check_RFID.h>
 #include <ControlUsers.h>
 #define button_PIN A0
-
 #define LED_OUTPUT D8
 
 bool access_allow=false;
 int pointer=0;
 int menu_screen=0;
 bool access_DHT=false;
-char IDWF[]="Blue Lagoon_Khach";
-char PASSWF[]="24bluelagoon";
+char IDWF[]="cuacao";
+char PASSWF[]="wifihuroi";
 
 
 button _button(button_PIN);
@@ -22,12 +21,14 @@ void check()
   _button.update_button(pointer,access_allow, menu_screen,temp, humid, WF_trig,WF_status,MQTT_status, MQTT_trig,MS_trig, Users_trig, allow_add_del);
 }
 
+
 void setup() 
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  pinMode(LED_OUTPUT,OUTPUT);
   while(!Serial);
-  mySerial.begin(115200);
-  delay(1000);
+  mySerial.begin(9600);
+  delay(2000);
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC52
   Wire.begin(D2,D3);  
@@ -36,6 +37,7 @@ void setup()
   lcd.backlight(); // Enable or Turn On the backlight 
   State.attach(0.1, check);
   Display(access_allow, pointer, menu_screen,false, false, false);
+  //digitalWrite(LED_OUTPUT,HIGH);
 }
 
 
@@ -43,6 +45,7 @@ void loop() {
   check_DHT();
   access_WF(IDWF, PASSWF, pointer);
   access_MQTT(pointer);
+  (WF_status)? digitalWrite(LED_OUTPUT,HIGH):digitalWrite(LED_OUTPUT,LOW);
   checkMaster(Users_trig,menu_screen, pointer);
   send_mess(pointer, MQTT_status,MS_trig);
   controlUser(allow_add_del, pointer);
